325cd630b5eada59dcf638bc485376ba1bec2c74
david-dede <davidenedede@gmail.com>
2024-02-17 18:52:23 +0100

    recursion

---
+++ Recursion/powerN.js
@@ -0,0 +1,17 @@
+// Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+// Examples
+
+// powerN(3, 1) → 3
+// powerN(3, 2) → 9
+// powerN(3, 3) → 27
+
+
+function powerN(base, n) {
+if(base===0||n===0){
+    return 0
+    return powerN(base)*powerN(n)
+}
+
+}
+console.log(powerN(3, 3));
---
+++ Recursion/test.js
@@ -0,0 +1,8 @@
+// function factorial(n) {
+// //   if (n <= 1) return n;
+// //   return n * factorial(n - 1);
+// // if(n===0)return "     stop"
+// return n+factorial(n+1)
+// }
+// console.table(factorial(5));
+console.log(4%2);
