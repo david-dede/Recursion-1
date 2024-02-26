2d7859f56e6392006db59830a770ddde1719878f
david-dede <davidenedede@gmail.com>
2024-02-26 20:59:57 +0100

    recursive

---
+++ changePi.js
@@ -0,0 +1,9 @@
+function changePi(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  if (str.substring(0, 2) === "pi") {
+    return "3.14" + changePi(str.substring(2));
+  }
+  return str.substring(0, 1) + changePi(str.substring(1));
+}
