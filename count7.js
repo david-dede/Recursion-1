03dbfa3ed87c92f60f15c9ccce4f46de11ff60e7
david-dede <davidenedede@gmail.com>
2024-03-28 23:06:52 +0100

    Update count7.js

--- count7.js
+++ count7.js
@@ -3,9 +3,9 @@ function count7(n,seven=[]) {
   if(n<7){return seven.length }
   let right=n%10
   if (right === 7) {
-    seven.push(right);
+    // seven.push(right);
   } else {
-    return count7(Math.floor(n / 10),seven);
+    // return count7(Math.floor(n / 10),seven);
   }
 
 }
