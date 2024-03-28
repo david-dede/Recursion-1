0a49720480fb06cee381a5cdfaf2037d79a29a6c
david-dede <davidenedede@gmail.com>
2024-03-28 23:07:19 +0100

    Update array11.js

--- array11.js
+++ array11.js
@@ -2,7 +2,7 @@ function array11(nums, i) {
   if (i >= nums.length) {
     return 0;
   }
-  if (nums[i] === 11) {
+  // if (nums[i] === 11) {
     return 1 + array11(nums, i + 1);
   }
   return array11(nums, i + 1);
