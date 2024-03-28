b535a155ac817fbbbe74dbcceac1cccf7194e8d1
david-dede <davidenedede@gmail.com>
2024-03-28 23:09:17 +0100

    Update changePi.js

--- changePi.js
+++ changePi.js
@@ -5,5 +5,5 @@ function changePi(str) {
   if (str.substring(0, 2) === "pi") {
     return "3.14" + changePi(str.substring(2));
   }
-  // return str.substring(0, 1) + changePi(str.substring(1));
+  return str.substring(0, 1) + changePi(str.substring(1));
 }
