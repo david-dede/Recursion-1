506b6d089954ed5c07a763fc8872ff14b4e350dd
david-dede <davidenedede@gmail.com>
2024-04-02 23:38:28 +0100

    Update countX.js

--- countX.js
+++ countX.js
@@ -1,9 +1,9 @@
-function countX(str) {
-  if (str.length === 0) {
-    return 0;
-  }
-  if (str[0] === "x") {
-    return 1 + countX(str.slice(1));
-  }
-  return countX(str.slice(1));
-}
+// function countX(str) {
+//   if (str.length === 0) {
+//     return 0;
+//   }
+//   if (str[0] === "x") {
+//     return 1 + countX(str.slice(1));
+//   }
+//   return countX(str.slice(1));
+// }
