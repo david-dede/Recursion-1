a4d938084d45817de124092f9d604a0a95e079fd
david-dede <davidenedede@gmail.com>
2024-04-02 23:38:20 +0100

    Update countHi.js

--- countHi.js
+++ countHi.js
@@ -1,9 +1,9 @@
-// function countHi(str) {
-//   if (str.length === 0) {
-//     return 0;
-//   }
-//   if (str.substring(0, 2) === "hi") {
-//     return 1 + countHi(str.substring(1));
-//   }
-//   return countHi(str.substring(1));
-// }
+function countHi(str) {
+  if (str.length === 0) {
+    return 0;
+  }
+  if (str.substring(0, 2) === "hi") {
+    return 1 + countHi(str.substring(1));
+  }
+  return countHi(str.substring(1));
+}
