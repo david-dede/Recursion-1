9a882207c673945645b463f1eaa6251478c9f89c
david-dede <davidenedede@gmail.com>
2024-04-03 23:04:50 +0100

    Update powerN.js

--- powerN.js
+++ powerN.js
@@ -1,17 +1,17 @@
-// // Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+// Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
-// // Examples
+// Examples
 
-// // powerN(3, 1) → 3
-// // powerN(3, 2) → 9
-// // powerN(3, 3) → 27
+// powerN(3, 1) → 3
+// powerN(3, 2) → 9
+// powerN(3, 3) → 27
 
 
-// function powerN(base, n) {
-// if(base===0||n===0){
-//     return 0
-//     return powerN(base)*powerN(n)
-// }
+function powerN(base, n) {
+if(base===0||n===0){
+    return 0
+    return powerN(base)*powerN(n)
+}
 
-// }
-// console.log(powerN(3, 3));
+}
+console.log(powerN(3, 3));
