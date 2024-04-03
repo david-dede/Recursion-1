19eb1a746c2c110b980489a1cddfb3b1bdc33d3a
david-dede <davidenedede@gmail.com>
2024-04-03 23:04:29 +0100

    Update noX.js

--- noX.js
+++ noX.js
@@ -1,9 +1,9 @@
-function noX(str) {
-  if (str.length === 0) {
-    return str;
-  }
-  if (str[0] === "x") return noX(str.slice(1));
-  else {
-    return str[0] + noX(str.substring(1));
-  }
-}
+// function noX(str) {
+//   if (str.length === 0) {
+//     return str;
+//   }
+//   if (str[0] === "x") return noX(str.slice(1));
+//   else {
+//     return str[0] + noX(str.substring(1));
+//   }
+// }
