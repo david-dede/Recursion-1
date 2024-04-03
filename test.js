8c57275b98b14e64bf6eebdb0a27b7aa2f51707f
david-dede <davidenedede@gmail.com>
2024-04-03 23:09:06 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-function array11(nums, i) {
-  if (nums >= 0) {
-    return 0;
-  }
-  if (nums[i] === 11) {
-    return 1 + array11(nums, i + 1);
-  }
-  return array11(nums, i + 1);
-}
+// function array11(nums, i) {
+//   if (nums >= 0) {
+//     return 0;
+//   }
+//   if (nums[i] === 11) {
+//     return 1 + array11(nums, i + 1);
+//   }
+//   return array11(nums, i + 1);
+// }
