0881c408b3e31ad22e7ae94fa726ed865f8402f3
david-dede <davidenedede@gmail.com>
2024-05-12 22:50:17 +0100

    Update array6.js

--- array6.js
+++ array6.js
@@ -2,7 +2,7 @@ function array6(nums, i) {
   if (i >= nums.length) {
     return false;
   }
-  if (nums[i] === 6) {
+  // if (nums[i] === 6) {
     // return true;
   // }
   // return array6(nums, i + 1);
